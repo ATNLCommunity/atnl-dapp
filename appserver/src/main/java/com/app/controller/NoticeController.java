8edbb26de8b6f5587ba369ffcd47d80057ad2b0b
zhangwen <364390162@qq.com>
2018-07-07 13:18:45 +0800

    new file:   appserver/src/main/java/com/app/controller/NoticeController.java 	new file:   appserver/src/main/java/com/app/model/Notice.java

---
+++ appserver/src/main/java/com/app/controller/NoticeController.java
@@ -0,0 +1,27 @@
+package com.app.controller;
+
+import com.app.model.Notice;
+
+
+import n.fw.base.BaseController;
+
+public class NoticeController extends BaseController
+{
+    /**
+     * 分页获取公告信息
+     */
+	public void getNotice()
+	{
+		int page = getParaToInt("page",0);
+		if(page <= 0)
+		{
+			page = 0;
+		}
+		else
+		{
+			page = page -1;
+		}
+		success(Notice.dao.list(page));
+	}
+	
+}
